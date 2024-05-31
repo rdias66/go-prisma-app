1dd27ee96dff6c99bd723ebe0a2cbf1aa299aa52
rdias66 <dias.a@live.com>
2024-05-30 23:33:05 -0300

    initial server setup

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -16,4 +18,17 @@ func main() {
 	}
 
 	fmt.Print("Server will be running on Port: " + os.Getenv("PORT"))
+
+	server := &http.Server{
+		Addr:           ":" + os.Getenv("PORT"),
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	server_error := server.ListenAndServe()
+
+	if server_error != nil {
+		panic(server_error)
+	}
 }
