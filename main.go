52f948b8732f3bcd247c200ee856f6aa99f69c2a
rdias66 <dias.a@live.com>
2024-05-30 23:27:58 -0300

    dot env added

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import("fmt")
+import (
+	"fmt"
+	"log"
+	"os"
 
-func main(){
-	fmt.Print("Start server!")
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Could not load environmental variables")
+	}
+
+	fmt.Print("Server will be running on Port: " + os.Getenv("PORT"))
 }
